6bf679f24a6d6b5dcd7dee369b621356176a966b
Malgorzata Haniszewska and Qianqian Hu <malgorzata@>
2017-10-19 12:05:39 -0700

    create a web that collect info from user and display a quiz result

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  $("form#person").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var height = parseFloat($("input#height").val());
+    var weight = parseInt($("input#weight").val());
 
-    if (age >=18) {
-      $('#adults').show();
+    if (age && weight && height) {
+      if (age < 18 || weight < 60 || height < 1.6) {
+        $("#cat").show();
+      } else if (age === 18) {
+        $("#cat, #dog").show();
+      } else {
+        $("#dog").show();
+      }
     } else {
-      $('#minors').show();
-    }
+      alert('Please enter all information.');
+      }
 
+    event.preventDefault();
+  });
 });
